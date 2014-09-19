e99a69dc4fd6b3f19aed5f736d563ee1a3859612
jmtoledo <jmtoledo@yahoo.com>
2014-09-19 17:19:06 +0200

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+## Author	: jmtoledo for the course R Programming - Assignemt 2
+## Date		: 17/09/2014
+##
+## For optimization purpose, we construct two functions that allow us
+## to caching the inverse of a matrix rather than computing it repeatedly,
+## and get its value when we need it.
+##
+## (Assume that the matrix supplied is always invertible.)
+##
+##
+## whith makeCacheMatrix creates the object and the functions 
+## to  calculate and store the value, in m object
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(inverse) m <<- inverse
+        getsolve <- function() m
+        list(set = set, get = get, setsolve = setsolve,
+             getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+## With cacheSolve gets the matrix inverse of x, 
+## 		 - if it was calculated before, simply get it 
+## 		 - if not, calculate it and store it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+##
+## Example in console: 
+##> a<-c(c(1,2,3),c(0,1,4),c(5,6,0))
+##> b<-matrix(a,3,3)
+##
+##> b
+##  	 [,1] [,2] [,3]
+##	[1,]    1    0    5
+##	[2,]    2    1    6
+##  [3,]    3    4    0
+##
+##> B_inv<-makeCacheMatrix(b)
+##
+##> B_inv$get
+##
+##> cacheSolve(B_inv)
+##  	 [,1] [,2] [,3]
+##	[1,]  -24   20   -5
+##	[2,]   18  -15    4
+##	[3,]    5   -4    1
+##> cacheSolve(B_inv)   
+##
+##getting cached data
+##     	 [,1] [,2] [,3]
+##	[1,]  -24   20   -5
+##	[2,]   18  -15    4
+##	[3,]    5   -4    1
+## so, inverse matrix was obtain from cached date instead of calculate ## if again
